fe38de3f902eeb23647c8927b250e32548912a5c
swati1751 <92619834+swati1751@users.noreply.github.com>
2022-01-06 08:40:54 +0530

    ScoreForCandidaterResponse.java

---
+++ ScoreForCandidaterResponse.java
@@ -0,0 +1,64 @@
+package practice.SwatiPractice;
+
+import java.util.Scanner;
+
+public class ScoreForCandidateResponse<CandidateResponse, CorrectAnswer> {
+
+    private String CandidateResponse;
+    private String CorrectAnswer;
+    private String Numbers;
+    private String words;
+
+    int num = 4;
+
+    private void String (CandidateResponse) {
+        this.CandidateResponse = CandidateResponse;
+    }
+    private void  String(CorrectAnswer){
+        this.CorrectAnswer = CorrectAnswer;
+    }
+
+    public void setCandidateResponse (String candidateResponse) {
+        CandidateResponse = candidateResponse;
+    }
+
+    public String getCandidateResponse () {
+        return Numbers;
+    }
+
+    public void setCorrectAnswer (String correctAnswer) {
+        CorrectAnswer = correctAnswer;
+    }
+
+    public String getCorrectAnswer () {
+        return words;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Please Enter Your words");
+        Scanner obj1=new Scanner(System.in);
+
+
+        int number;
+        if(int n=number;){
+        System.out.println("4 points");
+     }
+
+      else if("Word More Than 7 Chracter"){
+            System.out.println("3 points");
+
+        }
+
+        else if("Word Less han 5 Character") {
+            System.out.println("0 points");
+        }
+
+        else ("All Other Word") {
+            System.out.println("1 point");
+
+            System.out.println("Maximum Possible Score from the sum of points");
+            System.out.println("Points Scored Sum of points for each word in candidate response");
+        }
+    }
+}
